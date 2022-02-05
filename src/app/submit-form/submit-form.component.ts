7ed6e999ba1c784d49c44cc4de0359fd74d95322
NicholasHorta <nickdeveloper.js@gmil.com>
2022-02-05 18:08:13 +0000

    Submission form complete

--- src/app/submit-form/submit-form.component.ts
+++ src/app/submit-form/submit-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFirestore } from 'angularFire2/firestore';
+import { tap, first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-submit-form',
@@ -8,19 +10,43 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class SubmitFormComponent implements OnInit {
 
-  constructor(private fb: Form) { }
+  constructor(private fb: FormBuilder, private afs: AngularFirestore) { }
 
   myForm: any = FormGroup
 
+
+  loading: boolean = false;
+  success: boolean = false;
+
+
+
   ngOnInit(): void {
     this.myForm = this.fb.group({
-      email: ['', [Validators.email, Validators.required]],
+      email: ['', [Validators.required]],
       message: ['', [Validators.required]]
     })
   }
 
 
+  get email() {
+    return this.myForm.get('email');
+  }
+  get message() {
+    return this.myForm.get('message')
+  }
+
+
+  async submitHandler() {
+    this.loading = true;
+    const formValue = this.myForm.value
+
+    try {
+      await this.afs.collection('contacts').add(formValue);
+      this.success = true;
+    } catch (error) {
+      console.error(error)
+    }
+    this.loading = false;
+  }
+
 }
-// FormBuilder: Creates an AbstractControl from a user-specified configuration
-// Provides syntactic sugar that shortens creating an instance of a FormControl, FormGroup, or FormArray.
-// Reducing the amount of boilerplate needed to build complex forms
